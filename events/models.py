5bd8ef833a28a12650a6c21c2f71713b47e447c1
Stephen James <stephenorjames@gmail.com>
2016-05-16 00:00:23 -0400

    Started events index view

--- events/models.py
+++ events/models.py
@@ -1,22 +1,47 @@
+from datetime import datetime
+
 from django.db import models
+from django.utils.timezone import localtime
 
 
 class Event(models.Model):
     # What?
     title = models.CharField(max_length=50)
-    description = models.CharField(max_length=50)
+    description = models.TextField(max_length=500)
 
     # Where?
     location = models.CharField(max_length=50)
 
     # When?
-    start_date = models.DateField()
-    start_time = models.TimeField()
-    end_date = models.DateField(blank=True, null=True)
-    end_time = models.TimeField(blank=True, null=True)
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField(blank=True, null=True)
 
     # Who?
-    # TODO: contacts
+    # TODO: contacts/organizers
 
     def __str__(self):
-        return "%s (%s at %s)" % (self.title, self.start_date, self.start_time)
+        return "%(title)s (%(time)s)" % {
+            "title": self.title,
+            "time": " at ".join(Event.format_datetime(self.start_time))  # self.duration
+        }
+
+    @staticmethod
+    def format_datetime(dt):
+        date = datetime.strftime(localtime(dt), "%A, %B %d, %Y")
+        time = datetime.strftime(localtime(dt), "%I:%M %p")
+        yield from (date, time)
+
+    @property
+    def duration(self):
+        """Returns a string representing the duration of the event"""
+        dt = dict()
+        dt["start_date"], dt["start_time"] = Event.format_datetime(self.start_time)
+        if self.end_time:
+            dt["end_date"], dt["end_time"] = Event.format_datetime(self.end_time)
+            if self.start_time.date() == self.end_time.date():
+                duration = "%(start_date)s, from %(start_time)s to %(end_time)s"
+            else:
+                duration = "%(start_date)s at %(start_time)s to %(end_date)s at %(end_time)s"
+        else:
+            duration = "%(start_date)s at %(start_time)s"
+        return duration % dt
--- events/views.py
+++ events/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from datetime import datetime
 
-# Create your views here.
+from django.shortcuts import render
+from django.utils import timezone
+from events.models import Event
+
+
+def index(request):
+    events = Event.objects.all()
+
+    if request.GET.get("id"):
+        events = events.filter(id=request.GET["id"])
+    if request.GET.get("title"):
+        events = events.filter(title__contains=request.GET["title"])
+    if request.GET.get("after"):
+        try:
+            date_after = datetime.strptime(request.GET["after"], "%Y-%m-%d")
+        except ValueError:
+            pass
+        else:
+            date_after = timezone.make_aware(date_after, timezone.get_current_timezone())
+            events = events.filter(start_time__gte=date_after)
+    if request.GET.get("before"):
+        try:
+            date_before = datetime.strptime(request.GET["before"], "%Y-%m-%d")
+        except ValueError:
+            pass
+        else:
+            date_before = timezone.make_aware(date_before, timezone.get_current_timezone())
+            events = events.filter(end_time__lt=date_before)
+
+    return render(request, "events/events.html", {
+        "events": events,
+        "query": request.GET
+    })
